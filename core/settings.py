5d41e441dd11e00910ba8bbeb23d090783784558
Pablo <pablocfes@gmail.com>
2025-10-26 17:21:41 -0500

    Se ajusta allowed host

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'sintra.vanisa.cloud', '195.35.9.243', config('SERVER', default='127.0.0.1')]
 
 # Todos los proyecto usen BigAutoField por defecto
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
