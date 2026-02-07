83d360333626fe61b5ea4caa60e82b9bb6732b3d
Jeison Acevedo <smithacevedo2002@gmail.com>
2026-02-07 14:48:04 -0500

    feat: se agrega campo talla en modelo productos

--- apps/productos/models.py
+++ apps/productos/models.py
@@ -14,6 +14,7 @@ class Producto(models.Model):
     precio_venta = models.DecimalField(max_digits=10, decimal_places=2)
     linea = models.CharField(max_length=50, choices=LINEA_CHOICES, blank=True, null=True)
     descripcion = models.TextField()
+    talla = models.CharField(max_length=10, blank=True, null=True)
 
     def __str__(self):
         return self.referencia
